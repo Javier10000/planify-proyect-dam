d48e7892153f89e9df6756c2cedcea0c55f353d9
Javier10000 <fvermen203@iescarrillo.es>
2025-11-25 16:49:47 +0100

    TODO CONFIGURADO

---
+++ planify/src/app/components/auth/sigin/sigin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sigin',
+  imports: [],
+  templateUrl: './sigin.component.html',
+  styleUrl: './sigin.component.css'
+})
+export class SiginComponent {
+
+}
